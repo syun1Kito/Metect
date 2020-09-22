23ca85f2edac2aa9c6c65d2486a19274ab61ca7e
Syun1Kito <syun19991119@gmail.com>
2020-09-23 03:46:47 +0900

    基本的なピンボールの操作を実装

---
+++ Assets/Scripts/GameInstance.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstance
+{
+
+   
+    public int PlayerNum;                   //プレイヤーの人数1～2
+
+
+    GameInstance()
+    {        
+        init();
+    }
+    private static GameInstance _instance;
+
+    public static GameInstance Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new GameInstance();
+            return _instance;
+        }
+    }
+
+    public void init()
+    {
+        PlayerNum = 2;
+
+
+    }
+}
